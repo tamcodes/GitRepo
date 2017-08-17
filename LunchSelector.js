e7f28b20c395e55f3308cef5f333fe8489704cf3
Tam <tambuildsthings@gmail.com>
2017-08-17 13:13:36 +0100

    Pick a place for lunch with JS

---
+++ LunchSelector.js
@@ -0,0 +1,17 @@
+var items = Array(
+  "five guys",
+  "coffee works",
+  "falafel wrap",
+  "chinese buffet",
+  "afghan kitchen",
+  "mercer & co",
+  "waitrose",
+  "burrito",
+  "foodilic",
+  "pizza express",
+  "nandos"
+);
+
+var randFood = items[Math.floor(Math.random()*items.length)];
+
+alert(randFood);
